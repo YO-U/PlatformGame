c4b97a5ee9ebb9ad1714f2a5b6112a7932c7e656
Enoxy kek <enoxy7@gmail.com>
2023-01-08 17:36:06 +0200

    class Player

--- PlatformGame/PlatformGame.cpp
+++ PlatformGame/PlatformGame.cpp
@@ -2,20 +2,57 @@
 
 using namespace sf;
 
+class Player {
+public: float x, y, w, h, dx, dy, speed = 0;
+	  int dir = 0;
+	  String File;
+	  Image image;
+	  Texture texture;
+	  Sprite sprite;
+	  Player(String F, int X, int Y, float W, float H) {
+		  File = F;
+		  w = W;
+		  h = H;
+		  image.loadFromFile("Images/" + File);
+		  image.createMaskFromColor(Color(41, 33, 59));
+		  texture.loadFromImage(image);
+		  sprite.setTexture(texture);
+		  x = X;
+		  y = Y;
+		  sprite.setTextureRect(IntRect(w, h, w, h));
+	  }
+	  void update(float time) {
+		  switch (dir)
+		  {
+		  case 0:
+			  dx = speed;
+			  dy = 0;
+			  break;
+		  case 1:
+			  dx = -speed;
+			  dy = 0;
+			  break;
+		  case 2:
+			  dx = 0;
+			  dy = speed;
+			  break;
+		  case 3:
+			  dx = 0;
+			  dy = -speed;
+			  break;
+		  }
+		  x += dx * time;
+		  y += dy * time;
+		  speed = 0;
+		  sprite.setPosition(x, y);
+	  }
+};
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Test!");
 
-	Image heroImage;
-	heroImage.loadFromFile("Images/hero.png");
-
-	Texture heroTexture;
-	heroTexture.loadFromImage(heroImage);
-
-	Sprite heroSprite;
-	heroSprite.setTexture(heroTexture);
-	heroSprite.setTextureRect(IntRect(0,192,96, 96));
-	heroSprite.setPosition(50, 25);
+	Player p("hero.png", 250, 250, 96.0, 96.0);
 
 	float CurrentFrame = 0;
 	Clock clock;
@@ -32,33 +69,39 @@ int main()
 				window.close();
 		}
 		if (Keyboard::isKeyPressed(Keyboard::A)) {
+			p.dir = 1;
+			p.speed = 0.1;
 			CurrentFrame += 0.005 * time; 
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			heroSprite.move(-0.1 * time, 0);
-			heroSprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96));
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96));
+
 		}
 		if (Keyboard::isKeyPressed(Keyboard::D)) {
+			p.dir = 0;
+			p.speed = 0.1;
 			CurrentFrame += 0.005 * time;
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			heroSprite.move(0.1 * time, 0);
-			heroSprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::W)) {
+			p.dir = 3;
+			p.speed = 0.1;
 			CurrentFrame += 0.005 * time;
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			heroSprite.move(0, -0.1 * time);
-			heroSprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96));
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::S)) { 
+			p.dir = 2;
+			p.speed = 0.1;
 			CurrentFrame += 0.005 * time;
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			heroSprite.move(0, 0.1 * time);
-			heroSprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
 		}
 
+		p.update(time);
 
 		window.clear();
-		window.draw(heroSprite);
+		window.draw(p.sprite);
 		window.display();
 	}
 	return 0;
